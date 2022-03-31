d8c9ccdd7749b3b3aaed713d682b28679611def6
HenaFa <henry.fabritius@myy.haaga-helia.fi>
2022-03-31 10:58:32 +0300

    Tiim Scrum

---
+++ src/main/java/com/example/Tiim_Scrum_Projekti/TiimScrumProjektiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tiim_Scrum_Projekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiimScrumProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiimScrumProjektiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tiim_Scrum_Projekti/TiimScrumProjektiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tiim_Scrum_Projekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiimScrumProjektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
