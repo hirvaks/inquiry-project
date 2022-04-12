1f3d383996a2026af682f22ba63b859a6bec8f3d
HenaFa <henry.fabritius@myy.haaga-helia.fi>
2022-04-12 14:41:33 +0300

    Login toimivuus

--- src/main/java/com/example/Tiim_Scrum_Projekti/web/AdminController.java
+++ src/main/java/com/example/Tiim_Scrum_Projekti/web/AdminController.java
@@ -1,15 +1,21 @@
+package com.example.Tiim_Scrum_Projekti.web;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @CrossOrigin
 @Controller
-public class AdminController{
+public class AdminController {
 
-    @RequestMapping(value ={"/admin"}, method = RequestMethod.GET)
+	@RequestMapping(value = { "/admin" }, method = RequestMethod.GET)
 	public String getAdminPage() {
 		return "admin";
+	}
+
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
 	}
 }
