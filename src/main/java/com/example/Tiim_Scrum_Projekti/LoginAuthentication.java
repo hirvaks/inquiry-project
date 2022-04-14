d483aa05ff393818a7e4edb541c01290bf9afb69
HenaFa <henry.fabritius@myy.haaga-helia.fi>
2022-04-14 11:45:37 +0300

    Login toimivuus korjattu

--- src/main/java/com/example/Tiim_Scrum_Projekti/LoginAuthentication.java
+++ src/main/java/com/example/Tiim_Scrum_Projekti/LoginAuthentication.java
@@ -1,17 +1,24 @@
 package com.example.Tiim_Scrum_Projekti;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -26,9 +33,25 @@ public class LoginAuthentication extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .defaultSuccessUrl("/login_success")
+                .successForwardUrl("/login_success_handler")
+                .failureForwardUrl("/login_failure_handler")
                 .and()
                 .logout()
-                .permitAll();
+                .logoutUrl("/doLogout")
+                .logoutSuccessUrl("/logout_success")
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+                            Authentication authentication)
+                            throws IOException, ServletException {
+
+                        System.out.println("This user logged out: " + authentication.getName());
+
+                        response.sendRedirect("/logout_success");
+                    }
+                });
     }
 
     @Bean
--- src/main/java/com/example/Tiim_Scrum_Projekti/web/AdminController.java
+++ src/main/java/com/example/Tiim_Scrum_Projekti/web/AdminController.java
@@ -2,6 +2,7 @@ package com.example.Tiim_Scrum_Projekti.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,6 +17,20 @@ public class AdminController {
 
 	@RequestMapping(value = "/login")
 	public String login() {
+		return "login";
+	}
+
+	@PostMapping("/login_success_handler")
+	public String loginSuccessHandler() {
+		System.out.println("Logging user login success...");
+
+		return "admin";
+	}
+
+	@PostMapping("/login_failure_handler")
+	public String loginFailureHandler() {
+		System.out.println("Login failure handler....");
+
 		return "login";
 	}
 }
