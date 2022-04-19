638c431d82264495658c408154a63f5559859c99
vivkiri <elina.elkama@hotmail.com>
2022-04-19 13:30:41 +0300

    logout viesti kyselysivu

--- src/main/java/com/example/Tiim_Scrum_Projekti/web/AdminController.java
+++ src/main/java/com/example/Tiim_Scrum_Projekti/web/AdminController.java
@@ -2,7 +2,6 @@ package com.example.Tiim_Scrum_Projekti.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AdminController {
 
-	@RequestMapping(value = { "/admin" }, method = RequestMethod.GET)
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
 	public String getAdminPage() {
 		return "admin";
 	}
@@ -20,17 +19,8 @@ public class AdminController {
 		return "login";
 	}
 
-	@PostMapping("/login_success_handler")
-	public String loginSuccessHandler() {
-		System.out.println("Logging user login success...");
-
-		return "admin";
-	}
-
-	@PostMapping("/login_failure_handler")
-	public String loginFailureHandler() {
-		System.out.println("Login failure handler....");
-
-		return "login";
+	@RequestMapping(value = "/quizzes")
+	public String getQuizzes(){
+		return "quizzes";
 	}
 }
